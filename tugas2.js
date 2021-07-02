f2eaaed52a59d4e0950a5f4f17d99c41e188164f
Saddam Husein <saddambinhusein2@gmail.com>
2021-07-02 17:21:11 +0800

    Tugas 2 JS
    Pengumpulan tugas 2 JS ke Niomic

---
+++ tugas2.js
@@ -0,0 +1,14 @@
+let angka = '10';
+let teks = 'kata';
+let kondisi = true;
+let kosong = null;
+let tiada;
+let data = [1, 2, 3, 'empat', 5, null];
+
+console.log(angka);
+console.log(teks);
+console.log(kondisi);
+console.log(kosong);
+console.log(tiada);
+console.log(data);
+console.log(data[3]);
